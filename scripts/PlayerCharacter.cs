07268cda5beb6d9848cdb5eb4d4d1a9db48bdeca
Maraxoni <maraxoni16@gmail.com>
2024-04-24 23:52:05 +0200

    Added selecting and movement

--- scripts/PlayerCharacter.cs
+++ scripts/PlayerCharacter.cs
@@ -1,27 +1,62 @@
 using Godot;
 using System;
 
-public partial class PlayerCharacter
+public partial class PlayerCharacter : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-
-	public void _movement(double delta)
+	[Export]
+	public int Speed { get; set; } = 200;
+	
+	private Vector2 _target;
+	
+	public bool is_selected = false;
+	public bool is_moving = false;
+	
+	public override void _Ready()
 	{
-		//Vector2 velocity = Velocity;
-//
-		//// Handle Jump.
-		//
-		//if (direction != Vector2.Zero)
-		//{
-			//velocity.X = direction.X * Speed;
-		//}
-		//else
-		//{
-			//velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		//}
-//
-		//Velocity = velocity;
+		_target = Position; // Set initial target position to the character's starting position
+	}
+	
+	public override void _Input(InputEvent @event)
+	{
+		if (is_selected)
+		{
+			if (@event.IsActionPressed("right_click"))
+			{
+				_target = GetGlobalMousePosition();
+				is_moving = true;
+			}
+			if (@event.IsActionPressed("left_click"))
+			{
+				is_selected = false;
+			}
+		}	
+	}
+	private void _on_area_2d_input_event(Node viewport, InputEvent @event, long shape_idx)
+	{
+		if (@event.IsActionPressed("left_click"))
+		{
+			is_selected = true;
+		}
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Velocity = Position.DirectionTo(_target) * Speed;
+		// LookAt(_target);
+		if (is_moving)
+		{
+			Velocity = Position.DirectionTo(_target) * Speed;
+			// LookAt(_target);
+			if (Position.DistanceTo(_target) > 10)
+			{
+				MoveAndSlide();
+			}
+			else
+			{
+				is_moving = false;
+			}
+		}
 	}
 }
+
+
