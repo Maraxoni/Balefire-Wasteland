b3ab7df5366993f543d29e1427cf09efb515a983
Maraxoni <maraxoni16@gmail.com>
2024-04-25 02:31:25 +0200

    Wip 1

--- scripts/PlayerCharacter.cs
+++ scripts/PlayerCharacter.cs
@@ -11,9 +11,14 @@ public partial class PlayerCharacter : CharacterBody2D
 	public bool is_selected = false;
 	public bool is_moving = false;
 	
+	private Control outline;
+	
 	public override void _Ready()
 	{
 		_target = Position; // Set initial target position to the character's starting position
+		
+		outline = new Control();
+		AddChild(outline);
 	}
 	
 	public override void _Input(InputEvent @event)
@@ -55,6 +60,24 @@ public partial class PlayerCharacter : CharacterBody2D
 			{
 				is_moving = false;
 			}
+		}
+		// Update visibility of the outline based on selection
+		outline.Visible = is_selected;
+		
+	}
+	
+	public override void _Draw()
+	{
+		if (outline.Visible)
+		{
+			var color = new Color(1, 1, 1); // Outline color (white in this example)
+			var thickness = 2; // Outline thickness
+
+			// Get the bounding rectangle of the character sprite
+			Rect2 spriteRect = GetNode<Sprite2D>("Sprite2D").GetRect();
+			
+			// Draw the outline around the character sprite
+			outline.DrawRect(spriteRect, color, false, thickness);
 		}
 	}
 }
